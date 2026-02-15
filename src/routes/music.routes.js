0d96cd7b2c8c3afa1f70499b6f43182931317ad4
Rahul Kumar Sharma <63785638+rks1402@users.noreply.github.com>
2026-02-15 13:09:56 +0530

    added error handling in auth controller

--- src/routes/music.routes.js
+++ src/routes/music.routes.js
@@ -15,12 +15,7 @@ const upload = multer({ storage: multer.memoryStorage() });
 router.get("/", authMiddleware.authUser, getAllMusic);
 router.get("/album", authMiddleware.authUser, getAllAlbum);
 router.get("/album/:albumId", authMiddleware.authUser, getAlbumById);
-router.post(
-  "/upload",
-  authMiddleware.authArtist,
-  upload.single("file"),
-  createMusic,
-);
+router.post("/upload", authMiddleware.authArtist, upload.single("file"), createMusic);
 router.post("/create-album", authMiddleware.authArtist, createAlbum);
 
 module.exports = router;
